3c09640c1346532e9db739d79208944f8684b085
transfarmer <username11681@gmail.com>
2020-05-24 14:33:19 +0100

    remove unnecessary methods

--- src/main/java/transfarmer/anvil/event/EventListener.java
+++ src/main/java/transfarmer/anvil/event/EventListener.java
@@ -7,35 +7,23 @@ public class EventListener<E> implements Comparable<EventListener<? extends Even
     protected final Class<E> eventClass;
     protected final Consumer<E> consumer;
     protected final int priority;
-    protected final boolean persistence;
+    protected final boolean isPersistent;
 
     public EventListener(final Class<E> eventClass, final Consumer<E> consumer, final int priority,
-                            final boolean persistence) {
+                            final boolean isPersistent) {
         this.eventClass = eventClass;
         this.consumer = consumer;
         this.priority = priority;
-        this.persistence = persistence;
-    }
-
-    public Class<E> getEventClass() {
-        return this.eventClass;
-    }
-
-    public Consumer<E> getConsumer() {
-        return this.consumer;
-    }
-
-    public int getPriority() {
-        return this.priority;
+        this.isPersistent = isPersistent;
     }
 
     public boolean isPersistent() {
-        return this.persistence;
+        return this.isPersistent;
     }
 
     @Override
     public boolean equals(final Object other) {
-        return other instanceof EventListener && ((EventListener<?>) other).getEventClass() == this.getEventClass();
+        return other instanceof EventListener && ((EventListener<?>) other).eventClass == this.eventClass;
     }
 
     @Override
